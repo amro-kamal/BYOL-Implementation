cb6ac8591ecb0d07252029a9e9fe85743960ae4c
Amro Abbas <37993690+amro-kamal@users.noreply.github.com>
2021-07-26 16:04:59 +0000

    first commit

---
+++ knn_predictor.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
+import math
+# This for kNN prediction is from here:
+# https://github.com/IgorSusmelj/barlowtwins/blob/main/utils.py
+
+def knn_predict(feature, feature_bank, feature_labels, classes: int, knn_k: int, knn_t: float):
+    """Helper method to run kNN predictions on features based on a feature bank
+    Args:
+        feature: Tensor of shape [N, D] consisting of N D-dimensional features
+        feature_bank: Tensor of a database of features used for kNN
+        feature_labels: Labels for the features in our feature_bank
+        classes: Number of classes (e.g. 10 for CIFAR-10)
+        knn_k: Number of k neighbors used for kNN
+        knn_t: 
+    """
+    # compute cos similarity between each feature vector and feature bank ---> [B, N]
+    sim_matrix = torch.mm(feature, feature_bank)
+    # [B, K]
+    sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
+    # [B, K]
+    sim_labels = torch.gather(feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices)
+    # we do a reweighting of the similarities 
+    sim_weight = (sim_weight / knn_t).exp()
+    # counts for each class
+    one_hot_label = torch.zeros(feature.size(0) * knn_k, classes, device=sim_labels.device)
+    # [B*K, C]
+    one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
+    # weighted score ---> [B, C]
+    pred_scores = torch.sum(one_hot_label.view(feature.size(0), -1, classes) * sim_weight.unsqueeze(dim=-1), dim=1)
+    pred_labels = pred_scores.argsort(dim=-1, descending=True)
+    return pred_labels
+
+
+class BenchmarkModule(pl.LightningModule):
+    """A PyTorch Lightning Module for automated kNN callback
+    
+    At the end of every training epoch we create a feature bank by inferencing
+    the backbone on the dataloader passed to the module. 
+    At every validation step we predict features on the validation data.
+    After all predictions on validation data (validation_epoch_end) we evaluate
+    the predictions on a kNN classifier on the validation data using the 
+    feature_bank features from the train data.
+    We can access the highest accuracy during a kNN prediction using the 
+    max_accuracy attribute.
+    """
+    def __init__(self, dataloader_kNN, gpus, classes, knn_k, knn_t):
+        super().__init__()
+        self.resnet = nn.Module()
+        self.max_accuracy = 0.0
+        self.dataloader_kNN = dataloader_kNN
+        self.gpus = gpus
+        self.classes = classes
+        self.knn_k = knn_k
+        self.knn_t = knn_t
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop. It is independent of forward
+        self.step+=1
+        (x1,x2), y = batch
+        z1 , z2  = self.online_resnet(x1), self.target_resnet(x2) # projections, n-by-d
+        p1  = self.predictor(z1) # predictions, n-by-d
+
+        loss= self.mse_loss(p1, z2) # loss
+        #TODO:update the target network
+        for target_params, online_params in zip(self.target_resnet.parameters(), self.online_resnet.parameters()):
+            target_params = target_params * self.taw + (1-self.taw) * online_params
+        #TODO: update taw
+        self.taw=1- (1-args.taw) * (math.cos(math.pi*(k/K)) + 1)/2
+        self.step+=1
+
+        self.log('train_loss', loss)
+        return loss
+
+    def training_epoch_end(self, outputs):
+        # update feature bank at the end of each training epoch
+        self.resnet.eval()
+        self.feature_bank = []
+        self.targets_bank = []
+        with torch.no_grad():
+            for data in self.dataloader_kNN:
+                # img, target, _ = data
+                img, target = data
+                if self.gpus > 0:
+                    img = img.cuda()
+                    target = target.cuda()
+                feature = self.resnet(img).squeeze()
+                feature = F.normalize(feature, dim=1)
+                self.feature_bank.append(feature)
+                self.targets_bank.append(target)
+        self.feature_bank = torch.cat(self.feature_bank, dim=0).t().contiguous()
+        self.targets_bank = torch.cat(self.targets_bank, dim=0).t().contiguous()
+        self.resnet.train()
+
+    def validation_step(self, batch, batch_idx):
+        # we can only do kNN predictions once we have a feature bank
+        if hasattr(self, 'feature_bank') and hasattr(self, 'targets_bank'):
+            # images, targets, _ = batch
+            images, targets = batch
+            feature = self.resnet(images).squeeze()
+            feature = F.normalize(feature, dim=1)
+            pred_labels = knn_predict(feature, self.feature_bank, self.targets_bank, self.classes, self.knn_k, self.knn_t)
+            num = images.size(0)
+            top1 = (pred_labels[:, 0] == targets).float().sum().item()
+            return (num, top1)
+    
+    def validation_epoch_end(self, outputs):
+        if outputs:
+            total_num = 0
+            total_top1 = 0.
+            for (num, top1) in outputs:
+                total_num += num
+                total_top1 += top1
+            acc = float(total_top1 / total_num)
+            if acc > self.max_accuracy:
+                self.max_accuracy = acc
+            self.log('kNN_accuracy', acc * 100.0, prog_bar=True)
---
+++ main.py
@@ -0,0 +1,124 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+import torchvision
+import torch.nn.functional as F
+from pytorch_lightning import seed_everything, LightningModule, Trainer
+from pytorch_lightning.callbacks import EarlyStopping
+from torch import nn, optim, rand, sum as tsum, reshape, save
+from torch.utils.data import DataLoader, Dataset
+from pytorch_lightning.callbacks import LearningRateMonitor
+import pl_bolts
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+import argparse  
+from resnet import resnet18 
+import os
+from knn_predictor import BenchmarkModule
+from data import simsiam_cifar10_loader, cifar10_loader
+import copy
+
+parser = argparse.ArgumentParser(description='SimSiam Training')
+
+parser.add_argument('--epochs', default=1, type=int, help='number of total epochs to run')
+parser.add_argument('--batch-size', default=512, type=int, help='mini-batch size')
+parser.add_argument('--learning-rate', default=0.06, type=float,help='base learning rate')
+
+parser.add_argument('--weight-decay', default=5e-4, type=float, help='weight decay')
+
+parser.add_argument('--model-path',default=None)
+
+parser.add_argument('--knn-k', default=200, type=int)
+parser.add_argument('--knn-t', default=0.1, type=int)
+
+parser.add_argument('--in-dim', default=512, type=int)
+parser.add_argument('--h-dim', default=1024, type=int)
+parser.add_argument('--out-dim', default=256, type=int)
+parser.add_argument('--seed', default=42, type=int)
+parser.add_argument('--taw', default=0.996, type=float)
+
+parser.add_argument('--save-dir', default = '/gdrive/MyDrive/simsiam/', type=str)
+parser.add_argument('--exp-name', default = 'resnet_2048_512', type=str)
+
+args=parser.parse_args()
+
+logger = TensorBoardLogger(os.path.join(args.save_dir,args.exp_name), name="tensorboard")
+
+class SimSiam(BenchmarkModule):
+
+    def __init__(self, dataloader_kNN, gpus=1, classes=10, knn_k=args.knn_k, knn_t=args.knn_t, in_dim=args.in_dim,h_dim=args.h_dim,out_dim=args.out_dim,taw=args.taw, model_path=None):
+        super().__init__(dataloader_kNN, gpus, classes, knn_k, knn_t)
+        self.online_resnet = resnet18()   #torchvision.models.resnet18(pretrained=False)
+        self.target_resnet = resnet18()   #copy.copy(self.online_resnet)  
+        self.taw=taw 
+        self.step=0
+        self.total_steps=len(self.train_dataloader.sampler) * args.epochs
+        self.mse_loss=torch.MSELoss()
+
+        self.online_resnet.fc = nn.Sequential(nn.Linear(in_dim, h_dim, bias=False),  #projection [512x1024]
+                                        nn.BatchNorm1d(h_dim),
+                                        nn.ReLU(inplace=True), 
+                                        nn.Linear(h_dim, out_dim, bias=False),   #[1024x512]
+                                       ) 
+        self.target_resnet.fc = nn.Sequential(nn.Linear(in_dim, h_dim, bias=False),  #projection [512x1024]
+                                        nn.BatchNorm1d(h_dim),
+                                        nn.ReLU(inplace=True), 
+                                        nn.Linear(h_dim, out_dim, bias=False),   #[1024x512]
+                                       )
+
+        # build a 2-layer predictor
+        self.predictor = nn.Sequential(nn.Linear(out_dim, h_dim, bias=False),
+                                        nn.BatchNorm1d(h_dim),
+                                        nn.ReLU(inplace=True), 
+                                        nn.Linear(h_dim, out_dim)) 
+
+        if model_path and os.path.exists(model_path):
+          print('loading the model.....')
+          ckpt=torch.load(model_path)
+          self.online_resnet.load_state_dict(ckpt['online_resnet'])
+          self.predictor.load_state_dict(ckpt['predictor'])
+
+
+
+    def forward(self, x):
+        # in lightning, forward defines the prediction/inference actions
+        embedding = self.online_resnet(x)
+        return embedding
+
+    def D(self, p, z): # negative cosine similarity
+        z = z.detach() # stop gradient
+        p = F.normalize(p, dim=1) # l2-normalize each emb vector
+        z = F.normalize(z, dim=1) # l2-normalize each emb vector
+        return -(p*z).sum(dim=1).mean()
+
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.06, momentum=0.9, weight_decay=5e-4)
+        scheduler = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(optimizer=optimizer, warmup_epochs=10, max_epochs=800, warmup_start_lr=0.0, eta_min=0.0, last_epoch=- 1)
+        return [optimizer], [scheduler]
+
+if __name__ == '__main__':
+
+    log_dir = os.path.join(args.save_dir,args.exp_name,'tensorboard')
+    save_dir = os.path.join(args.save_dir,args.exp_name,'model.ckpt')
+
+    seed_everything(args.seed)
+
+    early_stop_callback = EarlyStopping(monitor='kNN_accuracy', min_delta=0.00, patience=5, verbose=True, mode='min')
+    checkpoint_callback = ModelCheckpoint( monitor='kNN_accuracy', dirpath= os.path.join(args.save_dir,args.exp_name,'checkpoints'), filename='online_resnet-epoch-{epoch}-acc-{kNN_accuracy:.2f}', mode='max')
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    knn_train_loader, knn_val_loader=cifar10_loader(args.batch_size)
+    simsiam = SimSiam(knn_train_loader,model_path=args.model_path)
+    trainer = Trainer( gpus=1, max_epochs=args.epochs, min_epochs=1, auto_lr_find=False, auto_scale_batch_size=False,
+                      progress_bar_refresh_rate=1,callbacks=[lr_monitor],logger=logger)
+
+    train_loader, _ = simsiam_cifar10_loader(args.batch_size)
+    
+    trainer.fit(simsiam, train_loader, knn_val_loader)
+    save({'online_resnet':simsiam.online_resnet.state_dict(),'predictor':simsiam.predictor.state_dict()},  save_dir)
